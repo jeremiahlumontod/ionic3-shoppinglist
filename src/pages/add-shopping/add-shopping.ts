afe47fc37d9eccf5699d0e830f28bc56ccf92823
Jeremiah Lumontod <jeremiah.lumontod@gmail.com>
2017-08-28 03:34:46 +0800

    add shopping item initial

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 
 
 @Component({
@@ -8,6 +11,10 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AddShoppingPage {
 
+  // Creating a new Object 
+  shoppingItem = {} as ShoppingItem;
+  shoppingItemRef$: FirebaseListObservable<ShoppingItem[]>
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -15,4 +22,22 @@ export class AddShoppingPage {
     console.log('ionViewDidLoad AddShoppingPage');
   }
 
+  addShoppingItem(shoppingItem: ShoppingItem) {
+    console.log(shoppingItem);
+    /*
+      Create a new anonymous object and convert itemNumber to a number.
+      Push this to our Firebase database under the 'shopping-list' node.
+    */
+    // this.shoppingItemRef$.push({
+    //   itemName: this.shoppingItem.itemName,
+    //   itemNumber: Number(this.shoppingItem.itemNumber)
+    // });
+
+    // Reset our ShoppingItem
+    // this.shoppingItem = {} as ShoppingItem;
+
+    // Navigate the user back to the ShoppingListPage
+    // this.navCtrl.pop();
+  }
+  
 }
