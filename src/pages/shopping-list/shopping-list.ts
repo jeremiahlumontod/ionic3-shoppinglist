95c89ba4f2f8a2faaa5910c4a2dd022827ca4e10
Jeremiah Lumontod <jeremiah.lumontod@gmail.com>
2017-08-28 04:29:37 +0800

    add edit delete initial

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 import { AddShoppingPage } from '../add-shopping/add-shopping';
@@ -17,11 +17,11 @@ export class ShoppingListPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private database: AngularFireDatabase) {
+    private database: AngularFireDatabase,
+    private actionSheetCtrl: ActionSheetController) {
     /*
       Pointing shoppingListRef$ at Firebase -> 'shopping-list' node.
-      That means not only can we push things from this reference to the database, but 
-      ALSO we have access to everything inside of that node. 
+      That means not only can we push things from this reference to the database, but ALSO we have access to everything inside of that node. 
     */
     this.shoppingListRef$ = this.database.list('shopping-list');
   }
@@ -33,6 +33,55 @@ export class ShoppingListPage {
   navigateToAddShoppingPage() {
     // Navigate the user to the AddShoppingPage
     this.navCtrl.push(AddShoppingPage);
+  }
+  
+
+
+  /*
+    Display an ActionSheet that gives the user the following options:
+
+    1. Edit the ShoppingItem
+    2. Delete the ShoppingItem
+    3. Cancel the selection
+  */
+  selectShoppingItem(shoppingItem: ShoppingItem) {
+    this.actionSheetCtrl.create({
+      title: `${shoppingItem.itemName}`,
+      buttons: [
+        {
+          text: 'Edit',
+          handler: () => {
+            // Send the user to the EditShoppingItemPage and pass the key as a parameter
+            this.navCtrl.push(EditShoppingItemPage,
+              { shoppingItemId: shoppingItem.$key });
+            
+            /*
+             Navigation stack:
+             
+              ['ShoppingListPage',
+               'EditShoppingItemPage',
+               { shoppingItemId: '-KowULdyLOK4ruWoKhws'}]
+            
+            */
+          }
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            // Delete the current ShoppingItem, passed in via the parameter
+            this.shoppingListRef$.remove(shoppingItem.$key);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log("The user has selected the cancel button");
+          }
+        }
+      ]
+    }).present();
   }
   
 }
